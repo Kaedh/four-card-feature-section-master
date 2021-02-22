bff2d4f858fd43ebbb6b6eec6c3868cf33035410
Kaedh <leonardomds2013@gmail.com>
2021-02-21 23:07:28 -0300

    project finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,55 @@
 import './App.css';
 import Card from '../Card/Card';
 
-function App() {
+import teamBuilderIcon from './assets/icon-team-builder.svg';
+import calculatorIcon from './assets/icon-calculator.svg';
+import karmaIcon from './assets/icon-karma.svg';
+import supervisorIcon from './assets/icon-supervisor.svg';
+
+const App = () => {
   return (
     <>
-    <h1>Hello World</h1>
-    <Card />
+      <header>
+        <div className="title">
+          <h1>Reliable, efficient delivery</h1>
+          <h1>Powered by Technology</h1>
+        </div>
+
+        <span>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</span>
+      </header>
+
+      <main>
+        <div className="card-wrapper">
+          <div className="card-container ">
+            <Card color="cyan" icon={supervisorIcon}>
+              <h1>Supervisor</h1>
+              <span>Monitors activity to identify project roadblocks</span>
+            </Card>
+          </div>
+          
+
+          <div className="card-container center">
+            <Card color="red" icon={teamBuilderIcon}>
+              <h1>Team Builder</h1>
+              <span>Scans our talent network to create the optimal team for your project</span>
+            </Card>
+
+            <Card color="orange" icon={karmaIcon}>
+              <h1>Karma</h1>
+              <span>Regularly evaluates our talent to ensure quality</span>
+            </Card>
+          </div>
+
+          <div className="card-container ">
+            <Card color="blue"  icon={calculatorIcon}>
+              <h1>Calculator</h1>
+              <span>Uses data from past projects to provide better delivery estimates</span>
+            </Card>
+          </div>
+        </div>
+      </main>
     </>
   );
-}
+};
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,14 @@
-const Card = () => {
-    return <h2>CARD</h2>
+import './Card.css'
+
+const Card = ({color, children, icon}) => {
+  return (
+    <div className="card">
+      <div className={"top " + color}> </div>
+      {children[0]}
+      {children[1]}
+      <img src={icon}></img>
+    </div>
+  );
 }
 
 export default Card;
